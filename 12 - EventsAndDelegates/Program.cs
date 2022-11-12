890fe6578bbb562ddb595c84e839c6e01d60c469
Aman Ahmed <aman16.aa17@gmail.com>
2022-11-12 14:22:51 +0100

    Delegates Basics

--- 12 - EventsAndDelegates/Program.cs
+++ 12 - EventsAndDelegates/Program.cs
@@ -1,16 +1,17 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Drawing;
 
 namespace _12___EventsAndDelegates
 {
     internal class Program
     {
-        // Properties
+        // // Properties
         // public string text;
         // public Color backColor;
         // public int sizeW, sizeH;
 
-        // Delegate
+        // // Delegate
         // private delegate void OnClickDelegate();
 
         // private OnClickDelegate onClick;
@@ -22,6 +23,14 @@ namespace _12___EventsAndDelegates
         //     // DisplayMessageSentDialog();
         // }
 
+        // // A method called Filter that takes a string
+        // static bool Filter(string s)
+        // {
+        //     // Return whether the string s contains the letter 'i'
+        //     // The Contains method will return a bool which we will return as well
+        //     return s.Contains("i");
+        // }
+
         static void Main(string[] args)
         {
             // SendButton.OnClick = SendButtonClick;
@@ -29,6 +38,26 @@ namespace _12___EventsAndDelegates
             // if (IsMouseHovering && IsLeftClickPressed)
             // {
             //     SendButton.OnClick();
+            // }
+
+            // // List of names
+            // List<string> names = new List<string>() { "Aiden", "Sif", "Walter", "Anatoli" };
+
+            // Console.WriteLine("----before----");
+            // // Print the names before the remove all method
+            // foreach (string name in names)
+            // {
+            //     Console.WriteLine(name);
+            // }
+
+            // // Calling RemoveAll and passing a method Filter we created
+            // names.RemoveAll(Filter);
+
+            // Console.WriteLine("----after----");
+            // // Print the names after the remove all method
+            // foreach (string name in names)
+            // {
+            //     Console.WriteLine(name);
             // }
         }
     }
