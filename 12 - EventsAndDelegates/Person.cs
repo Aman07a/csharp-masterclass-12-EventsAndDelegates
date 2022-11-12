a45811357a9f8da5c2f04ada43b31c363caed49c
Aman Ahmed <aman16.aa17@gmail.com>
2022-11-12 14:44:28 +0100

    Creating your own Delegates

---
+++ 12 - EventsAndDelegates/Person.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _12___EventsAndDelegates
+{
+    internal class Person
+    {
+        // Name of the property
+        public string Name { get; set; }
+
+        // Age of the property
+        public int Age { get; set; }
+
+    }
+}
--- 12 - EventsAndDelegates/Program.cs
+++ 12 - EventsAndDelegates/Program.cs
@@ -31,6 +31,45 @@ namespace _12___EventsAndDelegates
         //     return s.Contains("i");
         // }
 
+        // // Defining a delegate type called FilterDelegate that takes a person object and returns a bool
+        // public delegate bool FilterDelegate(Person p);
+
+        // // A method to display the list of people that passes the filter condition.
+        // // (Returns true)
+        // // This method will take a title to be displayed, the list of people.
+        // // And a filter delegate
+        // static void DisplayPeople(string title, List<Person> people, FilterDelegate filter)
+        // {
+        //     // Print the title
+        //     Console.WriteLine(title);
+
+        //     foreach (Person p in people)
+        //     {
+        //         // Check if this person passes the filter
+        //         if (filter(p))
+        //         {
+        //             // Print the person's name and age
+        //             Console.WriteLine("{0}, {1} years old.", p.Name, p.Age);
+        //         }
+        //     }
+        // }
+
+        // //==========FILTERS====================
+        // static bool IsMinor(Person p)
+        // {
+        //     return p.Age < 18;
+        // }
+
+        // static bool IsAdult(Person p)
+        // {
+        //     return p.Age >= 18;
+        // }
+
+        // static bool IsSenior(Person p)
+        // {
+        //     return p.Age >= 65;
+        // }
+
         static void Main(string[] args)
         {
             // SendButton.OnClick = SendButtonClick;
@@ -59,6 +98,19 @@ namespace _12___EventsAndDelegates
             // {
             //     Console.WriteLine(name);
             // }
+
+            // // Create 4 Person objects
+            // Person p1 = new Person() { Name = "Aiden", Age = 41 };
+            // Person p2 = new Person() { Name = "Sif", Age = 69 };
+            // Person p3 = new Person() { Name = "Walter", Age = 12 };
+            // Person p4 = new Person() { Name = "Anatoli", Age = 25 };
+
+            // // Add the objects to a list called people
+            // List<Person> people = new List<Person>() { p1, p2, p3, p4 };
+
+            // DisplayPeople("Kids: ", people, IsMinor);
+            // DisplayPeople("Adults: ", people, IsAdult);
+            // DisplayPeople("Seniors: ", people, IsSenior);
         }
     }
 }
