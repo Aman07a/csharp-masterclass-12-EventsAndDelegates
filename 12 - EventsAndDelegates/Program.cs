0d892dab1d1399b484a3744f0d56f061441ef5fa
Aman Ahmed <aman16.aa17@gmail.com>
2022-11-12 12:47:12 +0100

    Add project files.

---
+++ 12 - EventsAndDelegates/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _12___EventsAndDelegates
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
