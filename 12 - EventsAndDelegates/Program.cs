541d35ed78890ad0226501dfd89e3a0368eb5986
Aman Ahmed <aman16.aa17@gmail.com>
2022-11-12 14:10:48 +0100

    Delegates Introduction

--- 12 - EventsAndDelegates/Program.cs
+++ 12 - EventsAndDelegates/Program.cs
@@ -1,12 +1,35 @@
 ﻿using System;
+using System.Drawing;
 
 namespace _12___EventsAndDelegates
 {
     internal class Program
     {
+        // Properties
+        // public string text;
+        // public Color backColor;
+        // public int sizeW, sizeH;
+
+        // Delegate
+        // private delegate void OnClickDelegate();
+
+        // private OnClickDelegate onClick;
+
+        // void SendButtonClick()
+        // {
+        //     // Network.Connect();
+        //     // Network.SendMessage();
+        //     // DisplayMessageSentDialog();
+        // }
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            // SendButton.OnClick = SendButtonClick;
+
+            // if (IsMouseHovering && IsLeftClickPressed)
+            // {
+            //     SendButton.OnClick();
+            // }
         }
     }
 }
